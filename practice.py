b3bf3190488d0c73bf3daaa0d5b0552b5dae5301
samashtar <sam.ashtar@flatironschool.com>
2019-03-29 16:39:32 -0400

    practice 1

--- practice.py
+++ practice.py
@@ -1,1 +1,114 @@
-lol
+# # l ol
+
+# x = 1  # int
+# y = 2.5  # float
+# name = 'John Cena'  # string
+# is_cool = True  # capital for boolean
+
+# # Multiple assignment
+# x, y, name, is_cool = (1, 2.5, 'John Cena', True)
+
+# # Basic Math
+# a = x+y
+
+# # conversions
+# x = str(x)  # x is now a string
+# y = int(y)  # y is now an integer of 2, not float
+# z = float(y)  # y is now a float = 2.0
+
+
+# # STRINGS
+# name = 'Brad'
+# age = 37
+
+# # Concatenate
+# # can only concatenate of the same data type
+# # print('Hello, my name is ' + name + 'and I am' + str(age))
+
+# # interpolatation
+# # print('my name is {name} and I am {age}'.format(name=name, age=age))
+
+# # f-strings(only 3.6 + version)
+# # much more efficient than above
+# # print(f'Hello my name is {name} and I am {age}')
+
+# # String methods
+# s = 'hello world'
+
+# # will capitalize first letter
+# print(s.capitalize())
+
+# # will make all uppercase letters
+# print(s.upper())
+
+# # make all lowercase
+# print(s.lower())
+
+# # swap cases
+# print(s.swapcase())
+
+# # get length - any data type
+# print(len(s))
+
+# # # replace
+# # print(s.replace('world', 'everyone'))
+
+# # # counts how many of something in another thing
+# # # here counts how many h's in the s string
+# # sub = 'h'
+# # print(s.count(sub))
+
+# # # starts with - returns boolean
+# # print(s.startswith('hello'))
+
+# # # ends with
+# # print(s.endswith('d'))
+
+# # # split - same as javascript
+# # print(s.split())
+
+# # # find position of first
+# # print(s.find('r'))
+
+
+# # LISTS = array
+
+# # create a list
+# numbers = [1, 2, 3, 4, 5]
+# numbers2 = list((1, 2, 3, 4, 5))
+
+
+# # # get a value
+# # print(numbers[1])
+
+# # # get length
+# # print(len(numbers2))
+
+# # append to list
+# # numbers.append(6)
+# # print(numbers)
+
+# # remove from list
+# # numbers.remove(2)
+
+# # insert into position
+# # index, inserted thing
+# numbers.insert(2, 'inserted thing')
+
+# # remove at position 4
+# numbers.pop(4)
+
+# # reverse
+# numbers.reverse()
+
+# # sort
+# numbers.sort()
+
+# # reverse sort
+# numbers.sort(reverse=true)
+
+# # change value
+# numbers[0] = 'lol'
+
+
+# TUPLE
